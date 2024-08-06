9cc944a0ac8bd72e639506ab9b4f0c8f4d200a95
Aditya Yerudkar <127290435+adityayerudkar2311@users.noreply.github.com>
2024-08-06 18:38:15 +0000

    Conversion and  comparision operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 // "33" => 33
@@ -15,15 +15,47 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=+ truel 0 => false
 //""=> false
 //"aditya"=> true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumeber=String(someNumber)
-console.log(stringNumeber);
-console.log(typeof stringNumeber);
+//let stringNumeber=String(someNumber)
+//console.log(stringNumeber);
+//console.log(typeof stringNumeber);
+
+//*******************Operations **************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+3);
+//console.log(2-3);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 ="Hello"
+let str2 = "Aditya"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(+ true);
+//console.log(+ "");
+
+
+
+
+
 
